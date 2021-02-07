0b9208680e90adc2cacd9c5847b90f97cd711eec
NathanGarrihy <g00354922@gmit.ie>
2021-02-07 14:34:48 +0000

    Hex value demonstration complete

--- bitoperations.c
+++ bitoperations.c
@@ -2,6 +2,23 @@
 
 int main(int argc, char const *argv[])
 {
-    printf("%s\n", "I'm Nathan");
+    int i = 0xF1;
+
+    printf("Dec: %d\n", i);
+    printf("Hex: %X\n", i);
+
+    printf("Size of int: %d\n", sizeof(i));
+    printf("Size of char: %d\n", sizeof(char));
+
+    char c = 41;
+    printf("c in char is: %c\n", c);
+    printf("c in int is: %d\n", c);
+
+    char j = 1000000000;
+    printf("j in char is: %c\n", j);
+    printf("j in int is: %c\n", (char)j);
+    printf("j in int from char is: %d\n", (int) (char) j);
+
     return 0;
+    
 }
