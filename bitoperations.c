068708faa5e2c177bd5d90811cb1f7012f6eb70b
NathanGarrihy <g00354922@gmit.ie>
2021-02-08 15:53:46 +0000

    bit masks

--- bitoperations.c
+++ bitoperations.c
@@ -3,15 +3,15 @@
 void binPrint(unsigned int i)
 {
     // number of bits in an integer
-    int j = sizeof(int) * 8;
+    int j = sizeof(unsigned int) * 8;
 
     // temp variable
     int k;
 
-    // Loop over the number of bits in i
+    // Loop over the number of bits in i, left to right
     for (j--; j >= 0; j--)
     {
-        // 
+        // Pick out the j^th bit of i, check if t/f
         k = ((1 << j) & i) ? 1 : 0;
         // print k
         printf("%d", k);
@@ -21,20 +21,27 @@ void binPrint(unsigned int i)
 int main(int argc, char const *argv[])
 {
     // set i to literal value
-    unsigned int i = 0xffffffff; // 4294967295
+    unsigned int i = 0x0f0f0f0f; // 4294967295
 
-    printf("Original:  ");
+    printf("Original:\t");
     // returns 241 in decimal printed in
     // binary as a 32-bit integer
     binPrint(i);
-    printf("\t%X\t%u\n", i, i);
+    printf("\t%X\t%u\n\n", i, i);
 
-    for (int j = 0; j < 40; j++)
+    // 32
+    int j = sizeof(unsigned int) * 8;
+
+    for (j--; j >= 0; j--)
     {
-        printf("%3u << %2d ", i, j);
         // i shifted left j times
-        binPrint(i << j);
-        printf("\n");
+        binPrint(1 << j); printf("\n");
+        // printf("(1 << %3u) & %08x:", j, i);
+        // i
+        binPrint(i); printf("\n");
+        printf("-------------------------------- &\n");
+        // 1 shifted left j times bitwise logical and i
+        binPrint((1 << j) & i); printf("\n\n");
     }
 
     return 0;
