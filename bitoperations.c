05650119e66fcd7b6d89bed5220f80278afe15fe
NathanGarrihy <g00354922@gmit.ie>
2021-02-08 14:34:40 +0000

    added to understanding of bit operations

--- bitoperations.c
+++ bitoperations.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void binPrint(int i)
+void binPrint(unsigned int i)
 {
     // number of bits in an integer
     int j = sizeof(int) * 8;
@@ -8,26 +8,31 @@ void binPrint(int i)
     // temp variable
     int k;
 
+    // Loop over the number of bits in i
     for (j--; j >= 0; j--)
     {
+        // 
         k = ((1 << j) & i) ? 1 : 0;
+        // print k
         printf("%d", k);
     }
 }
 
 int main(int argc, char const *argv[])
 {
-    int i = 241;
+    // set i to literal value
+    unsigned int i = 0xffffffff; // 4294967295
 
     printf("Original:  ");
     // returns 241 in decimal printed in
     // binary as a 32-bit integer
     binPrint(i);
-    printf("\n");
+    printf("\t%X\t%u\n", i, i);
 
     for (int j = 0; j < 40; j++)
     {
-        printf("%3d << %2d ", i, j);
+        printf("%3u << %2d ", i, j);
+        // i shifted left j times
         binPrint(i << j);
         printf("\n");
     }
